729192fcaf0d2577c2146e998b51917ad69b5dcd
Amin PO Muar <helmi007@yahoo.com>
2016-04-10 16:34:22 +0800

    tambah lokasi css

--- Aplikasi/Papar/template/FlatAdmin-V.2/diatas.php
+++ Aplikasi/Papar/template/FlatAdmin-V.2/diatas.php
@@ -18,10 +18,20 @@ $url = URL . 'sumber/rangka-dawai/' . $template . '/';
     <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,900' rel='stylesheet' type='text/css'>
     <!-- CSS Utama -->
 	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css"/>
-	<link rel="stylesheet" type="text/css" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">	
+	<link rel="stylesheet" type="text/css" href="<?=$url?>dist/lib/css/bootstrap.min.css">
+	<!-- link rel="stylesheet" type="text/css" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" -->	
     <!-- CSS Libs -->
+	<link rel="stylesheet" type="text/css" href="<?=$url?>dist/lib/css/animate.min.css">
+    <link rel="stylesheet" type="text/css" href="<?=$url?>dist/lib/css/bootstrap-switch.min.css">
+    <link rel="stylesheet" type="text/css" href="<?=$url?>dist/lib/css/checkbox3.min.css">
+    <link rel="stylesheet" type="text/css" href="<?=$url?>dist/lib/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" type="text/css" href="<?=$url?>dist/lib/css/dataTables.bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="<?=$url?>dist/lib/css/select2.min.css">	
     <!-- CSS App -->
     <link rel="stylesheet" type="text/css" href="<?=$url?>css/style.css">
     <link rel="stylesheet" type="text/css" href="<?=$url?>css/themes/flat-blue.css">
 </head>
 <body class="flat-blue">
+<div class="app-container">
+<div class="row content-container">
+
