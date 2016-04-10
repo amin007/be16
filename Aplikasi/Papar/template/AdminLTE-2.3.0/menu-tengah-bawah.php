213edd83a78cd8c5a9ce5795b3b37ee049de7d6e
Amin PO Muar <helmi007@yahoo.com>
2016-04-10 10:54:13 +0800

    tambah fail menu_atas.php, menukiri.php, menu-tengah-atas.php, menu-tengah-bawah.php

---
+++ Aplikasi/Papar/template/AdminLTE-2.3.0/menu-tengah-bawah.php
@@ -0,0 +1,6 @@
+<?php echo "\r"; ?><?php if ($template==1):?>
+<?php else: # template asas?>
+	</section><!-- /.content -->
+	</div><!-- /.content-wrapper -->
+<!--  # tamat - menu-body ############################################################################################################# -->
+<?php endif; ?>
