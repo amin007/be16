7efaf4c3c6d94359f3a68c2e46a63902f27982d5
Amin PO Muar <helmi007@yahoo.com>
2016-04-10 08:31:06 +0800

    tambah fail dawairangka.php

---
+++ Aplikasi/Kawal/dawairangka.php
@@ -0,0 +1,41 @@
+<?php
+namespace Aplikasi\Kawal; //echo __NAMESPACE__; 
+class Dawairangka extends \Aplikasi\Kitab\Kawal
+{
+#==========================================================================================
+	function __construct() 
+	{
+		//echo '<br>class Dawairangka extends \Aplikasi\Kitab\Kawal';
+		parent::__construct();
+		//\Aplikasi\Kitab\Kebenaran::kawalMasuk();
+		$this->_folder = 'template';
+	}
+	
+	function index($template = null) 
+	{
+		# Set pemboleubah utama
+		$this->papar->Tajuk_Muka_Surat = 'Enjin Template';
+		$jenis = $this->pilihTemplate($template);
+		# pergi papar kandungan
+		$this->papar->bacaTemplate($this->_folder . '/index',
+			$jenis,0); # $noInclude=0
+	}
+#==========================================================================================		
+	function pilihTemplate($template)
+	{
+	#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^#		
+		switch ($template)
+		{
+/*			case 2:
+			break;
+//*/ 
+			default:
+			$jenis = 'AdminLTE-2.3.0';
+			break;
+		}
+	#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^#	
+		return $jenis;
+	}
+#==========================================================================================
+#==========================================================================================
+}
