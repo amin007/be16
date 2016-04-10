4039b86e65d0e2e18d29ca0ab05922d600a25b97
Amin PO Muar <helmi007@yahoo.com>
2016-04-10 12:07:19 +0800

    buang tatasusunan $kandunganMesej. asingkan menu_atas-mesej.php

--- Aplikasi/Papar/template/AdminLTE-2.3.0/menu_atas.php
+++ Aplikasi/Papar/template/AdminLTE-2.3.0/menu_atas.php
@@ -1,49 +1,3 @@
-<?php
-$kandunganMesej = array
-(
-	0 => array 
-	(
-		'url' => '#',
-		'userImage' => 'dist/img/user2-160x160.jpg',
-		'mesejTajuk' => 'Support Team 1',
-		'mesejMasa' => '5 mins',
-		'mesejIsi' => 'Why not buy a new awesome theme?',
-	),	
-	1 => array 
-	(
-		'url' => '#',
-		'userImage' => 'dist/img/user2-160x160.jpg',
-		'mesejTajuk' => 'Support Team 2',
-		'mesejMasa' => '10 mins',
-		'mesejIsi' => 'Sudah 10 minit',
-	),	
-	2 => array 
-	(
-		'url' => '#',
-		'userImage' => 'dist/img/user2-160x160.jpg',
-		'mesejTajuk' => 'Support Team 3',
-		'mesejMasa' => '15 mins',
-		'mesejIsi' => 'Sudah 15 minit',
-	),	
-	3 => array 
-	(
-		'url' => '#',
-		'userImage' => 'dist/img/user2-160x160.jpg',
-		'mesejTajuk' => 'Support Team 4',
-		'mesejMasa' => '20 mins',
-		'mesejIsi' => 'Sudah 20 minit',
-	),	
-	4 => array 
-	(
-		'url' => '#',
-		'userImage' => 'dist/img/user2-160x160.jpg',
-		'mesejTajuk' => 'Support Team 5',
-		'mesejMasa' => '25 mins',
-		'mesejIsi' => 'Sudah 25 minit',
-	),
-);
-
-?>
 <!-- # mula - menubar ############################################################################################################# -->
 	<!-- Main Header -->
 	<header class="main-header">
@@ -68,131 +22,93 @@ $kandunganMesej = array
 	<!-- Messages: style can be found in dropdown.less-->
 		<li class="dropdown messages-menu">
 		<!-- Menu toggle button --><?php
-		$bilMesej = 5; // nanti dapatkan count() dari array
-		echo "\t\t";
-		?><a href="#" class="dropdown-toggle" data-toggle="dropdown">
-		<i class="fa fa-envelope-o"></i>
-		<span class="label label-success"><?=$bilMesej?></span>
-		</a>
-		<ul class="dropdown-menu">
-		<li class="header">Anda ada <?=$bilMesej?> mesej</li>
-		<li><!-- inner menu: contains the messages -->
-			<ul class="menu">
-				<li><!-- start message -->
-				<a href="#">
-				<div class="pull-left">
-				<!-- User Image -->
-				<img src="<?=$url?>dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
-				</div>
-				<!-- Message title and timestamp -->
-				<h4>Support Team
-					<small><i class="fa fa-clock-o"></i> 5 mins</small>
-				</h4>
-				<!-- The message -->
-				<p>Why not buy a new awesome theme?</p>
-				</a>
-				</li><!-- end message -->
-			</ul><!-- /.menu -->
-		</li>
-		<!-- <li class="footer"><a href="#">See All Messages</a></li> -->
-		</ul>
+		include 'menu_atas-mesej.php';
+		echo "\t\t";?>
 		</li><!-- /.messages-menu -->
 
 	<!-- Notifications Menu -->
-	<li class="dropdown notifications-menu">
-	<!-- Menu toggle button -->
-	<a href="#" class="dropdown-toggle" data-toggle="dropdown">
-	<i class="fa fa-bell-o"></i>
-	<span class="label label-warning">10</span>
-	</a>
-	<ul class="dropdown-menu">
-	<li class="header">You have 10 notifications</li>
-	<li>
-	<!-- Inner Menu: contains the notifications -->
-	<ul class="menu">
-	<li><!-- start notification -->
-	<a href="#">
-	<i class="fa fa-users text-aqua"></i> 5 new members joined today
-	</a>
-	</li><!-- end notification -->
-	</ul>
-	</li>
-	<li class="footer"><a href="#">View all</a></li>
-	</ul>
-	</li>
-	<!-- Tasks Menu -->
-	<li class="dropdown tasks-menu">
-	<!-- Menu Toggle Button -->
-	<a href="#" class="dropdown-toggle" data-toggle="dropdown">
-	<i class="fa fa-flag-o"></i>
-	<span class="label label-danger">9</span>
-	</a>
-	<ul class="dropdown-menu">
-	<li class="header">You have 9 tasks</li>
-	<li>
-	<!-- Inner menu: contains the tasks -->
-	<ul class="menu">
-	<li><!-- Task item -->
-	<a href="#">
-	<!-- Task title and progress text -->
-	<h3>Design some buttons
-		<small class="pull-right">20%</small>
-	</h3>
-	<!-- The progress bar -->
-	<div class="progress xs">
-	<!-- Change the css width attribute to simulate progress -->
-	<div class="progress-bar progress-bar-aqua" style="width: 20%" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
-	<span class="sr-only">20% Complete</span>
-	</div>
-	</div>
-	</a>
-	</li><!-- end task item -->
-	</ul>
-	</li>
-	<li class="footer">
-	<a href="#">View all tasks</a>
-	</li>
-	</ul>
-	</li>
+		<li class="dropdown notifications-menu">
+		<!-- Menu toggle button -->
+		<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+		<i class="fa fa-bell-o"></i>
+		<span class="label label-warning">10</span>
+		</a>
+			<ul class="dropdown-menu">
+			<li class="header">You have 10 notifications</li>
+			<li>
+			<!-- Inner Menu: contains the notifications -->
+			<ul class="menu">
+			<li><!-- start notification -->
+			<a href="#">
+			<i class="fa fa-users text-aqua"></i> 5 new members joined today
+			</a>
+			</li><!-- end notification -->
+			</ul>
+			</li>
+			<li class="footer"><a href="#">View all</a></li>
+			</ul>
+		</li>
+
+		<!-- Tasks Menu -->
+		<li class="dropdown tasks-menu">
+		<!-- Menu Toggle Button -->
+		<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+		<i class="fa fa-flag-o"></i>
+		<span class="label label-danger">9</span>
+		</a>
+		<ul class="dropdown-menu">
+		<li class="header">You have 9 tasks</li>
+		<li><!-- Inner menu: contains the tasks -->
+			<ul class="menu">
+			<li><!-- Task item -->
+			<a href="#">
+			<!-- Task title and progress text -->
+			<h3>Design some buttons
+				<small class="pull-right">20%</small>
+			</h3>
+			<!-- The progress bar -->
+			<div class="progress xs">
+			<!-- Change the css width attribute to simulate progress -->
+			<div class="progress-bar progress-bar-aqua" style="width: 20%" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
+			<span class="sr-only">20% Complete</span>
+			</div>
+			</div>
+			</a>
+			</li><!-- end task item -->
+			</ul>
+		</li>
+		<li class="footer"><a href="#">View all tasks</a></li>
+		</ul>
+		</li>
+	
 	<!-- User Account Menu -->
 	<li class="dropdown user user-menu">
 	<!-- Menu Toggle Button -->
 	<a href="#" class="dropdown-toggle" data-toggle="dropdown">
-	<!-- The user image in the navbar-->
-	<img src="<?=$url?>dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
-	<!-- hidden-xs hides the username on small devices so only the image appears. -->
-	<span class="hidden-xs">Alexander Pierce</span>
+		<!-- The user image in the navbar-->
+		<img src="<?=$url?>dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
+		<!-- hidden-xs hides the username on small devices so only the image appears. -->
+		<span class="hidden-xs">{nama pengguna}</span>
 	</a>
 	<ul class="dropdown-menu">
 	<!-- The user image in the menu -->
-	<li class="user-header">
-	<img src="<?=$url?>dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
-	<p>Alexander Pierce - Web Developer
-		<small>Member since Nov. 2012</small>
-	</p>
-	</li>
-
-	<!-- Menu Body -->
-	<li class="user-body">
-	<div class="col-xs-4 text-center">
-	<a href="#">Followers</a>
-	</div>
-	<div class="col-xs-4 text-center">
-	<a href="#">Sales</a>
-	</div>
-	<div class="col-xs-4 text-center">
-	<a href="#">Friends</a>
-	</div>
-	</li>
-	<!-- Menu Footer-->
-	<li class="user-footer">
-	<div class="pull-left">
-	<a href="#" class="btn btn-default btn-flat">Profile</a>
-	</div>
-	<div class="pull-right">
-	<a href="#" class="btn btn-default btn-flat">Sign out</a>
-	</div>
-	</li>
+		<li class="user-header">
+		<img src="<?=$url?>dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+		<p>Alexander Pierce - Web Developer
+			<small>Member since Nov. 2012</small>
+		</p>
+		</li>
+		<!-- Menu Body -->
+		<li class="user-body">
+			<div class="col-xs-4 text-center"><a href="#">Followers</a></div>
+			<div class="col-xs-4 text-center"><a href="#">Sales</a></div>
+			<div class="col-xs-4 text-center"><a href="#">Friends</a></div>
+		</li>
+		<!-- Menu Footer-->
+		<li class="user-footer">
+			<div class="pull-left"><a href="#" class="btn btn-default btn-flat">Profile</a></div>
+			<div class="pull-right"><a href="#" class="btn btn-default btn-flat">Sign out</a></div>
+		</li>
 	</ul>
 	</li>
 	<!-- Control Sidebar Toggle Button -->
