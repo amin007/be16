4be6e68322af4b0b78c58a6c78ae569ae5696c8a
Amin PO Muar <helmi007@yahoo.com>
2016-04-10 16:44:34 +0800

    tukar lokasi js

--- Aplikasi/Papar/template/FlatAdmin-V.2/dibawah.php
+++ Aplikasi/Papar/template/FlatAdmin-V.2/dibawah.php
@@ -1,17 +1,17 @@
 <!-- Javascript Libs -->
-<script type="text/javascript" src="<?=$url?>js/jquery.min.js"></script>
-<script type="text/javascript" src="<?=$url?>js/bootstrap.min.js"></script>
-<script type="text/javascript" src="<?=$url?>js/Chart.min.js"></script>
-<script type="text/javascript" src="<?=$url?>js/bootstrap-switch.min.js"></script>
-<script type="text/javascript" src="<?=$url?>js/jquery.matchHeight-min.js"></script>
-<script type="text/javascript" src="<?=$url?>js/jquery.dataTables.min.js"></script>
-<script type="text/javascript" src="<?=$url?>js/dataTables.bootstrap.min.js"></script>
-<script type="text/javascript" src="<?=$url?>js/select2.full.min.js"></script>
-<script type="text/javascript" src="<?=$url?>js/ace/ace.js"></script>
-<script type="text/javascript" src="<?=$url?>js/ace/mode-html.js"></script>
-<script type="text/javascript" src="<?=$url?>js/ace/theme-github.js"></script>
+<script type="text/javascript" src="<?=$url?>dist/lib/js/jquery.min.js"></script>
+<script type="text/javascript" src="<?=$url?>dist/lib/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="<?=$url?>dist/lib/js/Chart.min.js"></script>
+<script type="text/javascript" src="<?=$url?>dist/lib/js/bootstrap-switch.min.js"></script>
+<script type="text/javascript" src="<?=$url?>dist/lib/js/jquery.matchHeight-min.js"></script>
+<script type="text/javascript" src="<?=$url?>dist/lib/js/jquery.dataTables.min.js"></script>
+<script type="text/javascript" src="<?=$url?>dist/lib/js/dataTables.bootstrap.min.js"></script>
+<script type="text/javascript" src="<?=$url?>dist/lib/js/select2.full.min.js"></script>
+<script type="text/javascript" src="<?=$url?>dist/lib/js/ace/ace.js"></script>
+<script type="text/javascript" src="<?=$url?>dist/lib/js/ace/mode-html.js"></script>
+<script type="text/javascript" src="<?=$url?>dist/lib/js/ace/theme-github.js"></script>
 <!-- Javascript -->
-<script type="text/javascript" src="<?=$url?>js/app.js"></script>
-<script type="text/javascript" src="<?=$url?>js/index.js"></script>
+<script type="text/javascript" src="<?=$url?>dist/js/app.js"></script>
+<script type="text/javascript" src="<?=$url?>dist/js/index.js"></script>
 </body>
 </html>
