c11dca29445e839d33a0be68bf7125a757a35980
Amin PO Muar <helmi007@yahoo.com>
2016-04-10 12:10:50 +0800

    tambah menu-footer

--- Aplikasi/Papar/template/AdminLTE-2.3.0/menu-tengah-bawah.php
+++ Aplikasi/Papar/template/AdminLTE-2.3.0/menu-tengah-bawah.php
@@ -3,4 +3,16 @@
 	</section><!-- /.content -->
 	</div><!-- /.content-wrapper -->
 <!--  # tamat - menu-body ############################################################################################################# -->
+<!--  # mula - menu-footer ############################################################################################################# -->
+	<!-- Main Footer -->
+	<footer class="main-footer">
+	<!-- Default to the left -->
+	<strong>Copyright &copy; 2016 | Template <a href="#"><?php echo $template ?></a></strong>
+	<!-- To the right -->
+	<div class="pull-right hidden-xs">
+	{Kanan ke mana}
+	</div>
+	</footer>
+<!--  # tamat - menu-footer ############################################################################################################# -->
+
 <?php endif; ?>
