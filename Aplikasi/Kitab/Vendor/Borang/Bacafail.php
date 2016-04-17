57843498daa927df3da6832526a0ba119cc35db6
Amin PO Muar <helmi007@yahoo.com>
2016-04-17 09:10:51 +0800

    buat fungsi untuk baca fail

--- Aplikasi/Kitab/Vendor/Borang/Bacafail.php
+++ Aplikasi/Kitab/Vendor/Borang/Bacafail.php
@@ -4,21 +4,14 @@ class Bacafail
 {
 #==========================================================================================
 	public function namafail()
-	{ # 
+	{
 		# baca fail kandungan utama
 		$url = 'http://localhost/sumber/startbootstrap-sb-admin-2-1.0.8/pages/s1.html';
-		$handle = fopen($url, "rb");
-		$fsize = 8192; //$fsize = filesize($url); 
-		if (FALSE === $handle) { exit("Failed to open stream to URL");}
-		$contents = '';
-		while (!feof($handle)) {$contents .= fread($handle, $fsize);}
-
+		list($membukaFail,$dataFail) = $this->bukafailutama($url);
+		
 		# baca fail menu atas
-		$menuAtas = 'http://http://localhost/sumber/startbootstrap-sb-admin-2-1.0.8/pages/s1-menu_atas.html';
-		$bacaMenuAtas = fopen($menuAtas, "rb");
-		if (FALSE === $bacaMenuAtas) { exit("Failed to open stream to URL"); }
-		$menuatas = '';
-		while (!feof($bacaMenuAtas)) {  $menuatas .= fread($bacaMenuAtas, $fsize);}
+		$url2 = 'http://http://localhost/sumber/startbootstrap-sb-admin-2-1.0.8/pages/s1-menu_atas.html';
+		list($bacaMenuAtas,$menuAtas) = $this->bukafailmenu($url2);
 
 		# ubahsuai data
 		$kandungan =  str_replace('{{ s1-menu-atas.php }}', $menuatas, $contents);
@@ -26,14 +19,36 @@ class Bacafail
 		$kandungan2 =  str_replace('{{ tengah.php }}', $tengah, $kandungan);
 
 		# pergi papar kandungan
-		//echo $contents;
-		//echo $kandungan;
+		//echo $contents; //echo $kandungan;
 		echo $kandungan2;
 
 		# tutup fail
-		fclose($handle);
+		fclose($dataFail);
 		fclose($bacaMenuAtas);		
 	}
+	
+	public function bukafailutama($url)
+	{
+		$membukaFail = fopen($url, "rb");
+		$saizFail = 8192; //$saizFail = filesize($url); 
+		if (FALSE === $membukaFail) { exit("Failed to open stream to URL");}
+		$dataFail = '';
+		while (!feof($membukaFail)) {$dataFail .= fread($membukaFail, $saizFail);}
+		
+		return array($membukaFail,$dataFail);
+	}
+	
+	public function bukafailmenu($url)
+	{
+		$bacaMenuAtas = fopen($url, "rb");
+		$saizFail = 8192; //$saizFail = filesize($url); 
+		if (FALSE === $bacaMenuAtas) { exit("Failed to open stream to URL"); }
+		$menuAtas = '';
+		while (!feof($bacaMenuAtas)) {  $menuAtas .= fread($bacaMenuAtas, $fsize);}
+		
+		return array($bacaMenuAtas,$menuAtas);
+	}
+	
 #==========================================================================================
 #==========================================================================================
 }
