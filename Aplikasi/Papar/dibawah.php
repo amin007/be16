ce4529a4c12afd0b89e7e49608b8814d4b0a0e21
Amin PO Muar <helmi007@yahoo.com>
2016-04-17 12:03:30 +0800

    tukar tahun 2014 ke 2016

--- Aplikasi/Papar/dibawah.php
+++ Aplikasi/Papar/dibawah.php
@@ -3,7 +3,7 @@
 <footer class="footer">
 	<div class="container">
 		<span class="label label-info">
-		&copy; Hak Cipta Terperihara 2014. Theme <?php 
+		&copy; Hak Cipta Terperihara 2016. Theme <?php 
 $theme = ($pilih==null) ? 'Asal Bootstrap Twitter' : $pilih;
 echo $theme = (isset($theme)) ? $theme : null;
 ?>	
@@ -11,6 +11,5 @@ echo $theme = (isset($theme)) ? $theme : null;
 	</div>
 </footer>
 <?php require 'jquery.php'; ?>
-
 </body>
 </html>
