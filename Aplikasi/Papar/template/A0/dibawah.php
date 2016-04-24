adecc0f0f8f74625caf964df2cdc901a33dd6dc2
Amin PO Muar <helmi007@yahoo.com>
2016-04-24 09:39:13 +0800

    tambah template/A0

---
+++ Aplikasi/Papar/template/A0/dibawah.php
@@ -0,0 +1,15 @@
+<!-- Footer
+================================================== -->
+<footer class="footer">
+	<div class="container">
+		<span class="label label-info">
+		&copy; Hak Cipta Terperihara 2016. Theme <?php 
+$theme = ( !isset($pilih) ) ? 'Asal Bootstrap Twitter' : $pilih;
+echo $theme = (isset($theme)) ? $theme : null;
+?>	
+		</span>
+	</div>
+</footer>
+<?php require 'jquery.php'; ?>
+</body>
+</html>
