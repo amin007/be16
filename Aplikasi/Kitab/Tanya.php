530712001815cc14fd79779f4f1e46fb2f09bc32
Amin PO Muar <helmi007@yahoo.com>
2016-04-24 09:54:42 +0800

    tukar cara panggil class yang melibatkan namespace

--- Aplikasi/Kitab/Tanya.php
+++ Aplikasi/Kitab/Tanya.php
@@ -5,9 +5,8 @@ class Tanya
 #==========================================================================================
 	function __construct() 
 	{
-		//$this->db = new DB_Pdo(DB_TYPE, DB_HOST, DB_NAME, DB_USER, DB_PASS);
+		//$this->db = new \Aplikasi\Kitab\DB_Pdo(DB_TYPE, DB_HOST, DB_NAME, DB_USER, DB_PASS);
 		$this->db = new \Aplikasi\Kitab\DB_Mysqli(DB_TYPE, DB_HOST, DB_NAME, DB_USER, DB_PASS);
-		//$this->crud = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 	}
 	
 	private function cariApa($fix,$atau,$medan,$cariApa,$akhir)
