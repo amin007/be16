b136b82280feecb8c49bb0f2cae21d6d0003a9e1
Amin PO Muar <helmi007@yahoo.com>
2016-04-24 15:21:36 +0800

    buang baris

--- Aplikasi/Kawal/index.php
+++ Aplikasi/Kawal/index.php
@@ -24,7 +24,6 @@ class Index extends \Aplikasi\Kitab\Kawal
 		//$this->papar->bacaTemplate(
 		//$this->papar->paparTemplate(
 			$this->_folder . '/login',$jenis,0); # $noInclude=0
-		
 	}
 #==========================================================================================	
 	function muar() 
