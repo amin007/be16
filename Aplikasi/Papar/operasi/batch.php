205f14c413792d07f85a2dbd9910b95171bacd3c
Amin PO Muar <helmi007@yahoo.com>
2016-04-27 13:45:46 +0800

    tukar koding fail operasi/batch.php sama macam prosesan/batch.php

--- Aplikasi/Papar/operasi/batch.php
+++ Aplikasi/Papar/operasi/batch.php
@@ -6,7 +6,7 @@ $target = null;
 //$target = ' target="_blank"';
 //echo '<pre>$senaraiStaf->'; print_r($senaraiStaf) . '</pre>';
 foreach ($senaraiStaf as $namaStaf):
-	$urlStaf .=  "<br>\r" . '<a' . $target . ' href="' . URL .'operasi/batch/' . $namaStaf . '">'
+	$urlStaf .=  "\r | " . '<a' . $target . ' href="' . URL .'operasi/batch/' . $namaStaf . '">'
 			 .  $namaStaf . '</a>';
 endforeach;
 $paparStaf = $this->namaPegawai . " ada dalam senarai staf";
@@ -14,83 +14,50 @@ $paparXStaf = $this->namaPegawai . " tiada dalam senarai staf.<br>"
 	. ' klik salah satu pautan staf di bawah ini '
 	. $urlStaf 
 	. '';
-?><?php if (($this->namaPegawai == null)) : ?>
-<div class="container">
-nama pegawai tidak wujud.
-klik salah satu pautan staf di bawah ini <?=$urlStaf?>
-</div><!-- / class="container" -->
-<?php elseif (($this->namaPegawai != null) && ($this->noBatch == null)) : ?>	
-<?php # set pembolehubah
+	
+if (($this->namaPegawai == null)) :
+	$notaTambahan = 'nama pegawai tidak wujud. klik salah satu pautan staf di bawah ini ' . $urlStaf;
+	$mencari = URL . 'operasi/tambahNamaStaf';
+	$butangHantar = 'Letak Nama Staf';
+elseif (($this->namaPegawai != null) && ($this->noBatch == null)) :
+	# set pembolehubah
 	$namaPegawai = (!isset($this->namaPegawai)) ? null : $this->namaPegawai;
-	$mencari = URL . 'operasi/tambahBatchBaru/' . $namaPegawai;?>
-<div class="container">
-	<h1><?php
-	echo '<small>' . $mencari . '</small><br>';
-	echo (in_array($this->namaPegawai,$senaraiStaf)) ? $paparStaf : $paparXStaf;
-	?></h1>
-
-	<div align="center"><form method="GET" action="<?=$mencari?>" class="form-inline" autocomplete="off">
-	<div class="form-group"><div class="input-group">
-		<input type="text" name="tambah" class="form-control" autofocus
-		id="inputString" onkeyup="lookup(this.value);" onblur="fill();">
-		<span class="input-group-addon">
-		<input type="submit" value="Letak No Batch">
-		</span>
-	</div></div>
-	<div class="suggestionsBox" id="suggestions" style="display: none; " >
-		<div class="suggestionList" id="autoSuggestionsList">&nbsp;</div>
-	</div>
-	</form></div><br>
-
-</div><!-- / class="container" -->
-<?php elseif (($this->namaPegawai != null) 
-	&& ($this->noBatch != null)
-	&& ($this->error == 'Kosong') ) : ?><?php 
+	$mencari = URL . 'operasi/tambahBatchBaru/' . $namaPegawai;
+	$notaTambahan = '<small>' . $mencari . '</small><br>' .
+	( (in_array($this->namaPegawai,$senaraiStaf)) ? $paparStaf : $paparXStaf );
+	$butangHantar = 'Letak No Batch';
+elseif (($this->namaPegawai != null) && ($this->noBatch != null)
+	&& ($this->error == 'Kosong') ) : 
 	# set pembolehubah
 	$namaPegawai = (!isset($this->namaPegawai)) ? null : $this->namaPegawai;
 	$cariBatch = (!isset($this->noBatch)) ? null : $this->noBatch;
-	$mencari = URL . 'operasi/ubahBatchProses/' . $namaPegawai . '/' . $cariBatch; ?>
-<div class="container">
-	<h1><?php
-	echo '<small>' . $mencari . '</small><br>';
-	echo 'Daftar kes masing-masing<br>';
-	//echo (in_array($this->namaPegawai,$senaraiStaf)) ? $paparStaf : $paparXStaf;
-	?></h1>
-
-	<div align="center"><form method="GET" action="<?=$mencari?>" class="form-inline" autocomplete="off">
-	<div class="form-group"><div class="input-group">
-		<input type="text" name="cari" class="form-control" autofocus
-		id="inputString" onkeyup="lookup(this.value);" onblur="fill();">
-		<span class="input-group-addon">
-		<input type="submit" value="Letak No ID">
-		</span>
-	</div></div>
-	<div class="suggestionsBox" id="suggestions" style="display: none; " >
-		<div class="suggestionList" id="autoSuggestionsList">&nbsp;</div>
-	</div>
-	</form></div><br>
-
-</div><!-- / class="container" -->
-<?php else : ?><?php # set pembolehubah
+	$mencari = URL . 'operasi/ubahBatchProses/' . $namaPegawai . '/' . $cariBatch; 
+	$notaTambahan = '<small>' . $mencari . '</small><br>' . "\r" .
+	'Daftar kes masing-masing<br>';
+	$butangHantar = 'Letak No ID';
+else : ?><?php # set pembolehubah
 	$namaPegawai = (!isset($this->namaPegawai)) ? null : $this->namaPegawai;
 	$cariBatch = (!isset($this->noBatch)) ? null : $this->noBatch;
 	$paparError = (!isset($this->error)) ? null : $this->error;
 	$mencari = URL . 'operasi/ubahBatchProses/' . $namaPegawai . '/' . $cariBatch;
 	$cetakF10 = URL . 'laporan/cetakf10/' . $cariBatch . '/1000';
 	$cetakA1 = URL . 'laporan/cetakA1/' . $cariBatch . '/1000';
-?>
-<div class="container">
-	<h3><a target="_blank" href="<?=$cetakF10?>"> Cetak F10</a>|
-	<a target="_blank" href="<?=$cetakF10?>">Cetak A1</a></h3>
-	<h1>Ubah | Nama Pegawai : <?=$namaPegawai?> | BatchOperasi : <?=$cariBatch?><br>
-	<small>Nota: <?=$paparError?></small></h1>
+	$cetak = '<h3><a target="_blank" href="' . $cetakF10 . '"> Cetak F10</a>| ' . "\r" .
+	'<a target="_blank" href="' . $cetakA1 . '">Cetak A1</a></h3>';
+	$notaTambahan = 'Ubah | Nama Pegawai : ' . $namaPegawai . ' | BatchOperasi : ' . $cariBatch . '<br>' . "\r" .
+	'<small>Nota: ' . $paparError . '</small>';
+	$butangHantar = 'Kemaskini';
+endif; ?>
+<div class="container"><?php echo (!isset($cetak)) ? null : "\r$cetak" ?>
+	<h1><?=$notaTambahan?></h1>
 
 	<div align="center"><form method="GET" action="<?=$mencari?>" class="form-inline" autocomplete="off">
+	<?php echo $mencari . '<br>' ?>
 	<div class="form-group"><div class="input-group">
 		<input type="text" name="cari" class="form-control" autofocus
 		id="inputString" onkeyup="lookup(this.value);" onblur="fill();">
 		<span class="input-group-addon">
-		<input type="submit" value="Kemaskini">
+		<input type="submit" value="<?=$butangHantar?>">
 		</span>
 	</div></div>
 	<div class="suggestionsBox" id="suggestions" style="display: none; " >
@@ -100,7 +67,8 @@ klik salah satu pautan staf di bawah ini <?=$urlStaf?>
 
 </div><!-- / class="container" -->
 <!-- mula - baca jadual berulang ///////////////////////////////////////////////////////////////////////// -->
-<?php include 'papar_jadual_berulang.php'; ?>
+<?php 
+if (isset($this->cariApa) )
+	include 'papar_jadual_berulang.php'; 
+?>
 <!-- tamat - baca jadual berulang //////////////////////////////////////////////////////////////////////// -->
-<?php endif; ?>
-
