33f781b420f24e117dc2ef1600c44c983a1e0b5d
Amin PO Muar <helmi007@yahoo.com>
2016-04-28 16:55:41 +0800

    tukar // kepada #

--- Aplikasi/Kitab/Route.php
+++ Aplikasi/Kitab/Route.php
@@ -77,47 +77,40 @@ class Route
     {
         $panjang = count($url); //echo '$panjang=' . $panjang . '<br>';
  
-        // Pastikan kaedah yang kita panggil wujud
+        # Pastikan kaedah yang kita panggil wujud
         if ($panjang > 1)
         {
 			if (!method_exists($kawal, $url[1])) {$this->parameter();}
 		}
 			
-			// Tentukan apa yang dimuatkan		
+			# Tentukan apa yang dimuatkan		
 			switch ($panjang)
 			{
-				case 8:
-				//Kawal->Kaedah(Param2, Param3, Param4, Param5)
+				case 8: # Kawal->Kaedah(Param2, Param3, Param4, Param5)
 				$kawal->{$url[1]}($url[2], $url[3], $url[4], $url[5], $url[6], $url[7]);
 				break;
 
-				case 7:
-				//Kawal->Kaedah(Param2, Param3, Param4, Param5)
+				case 7: # Kawal->Kaedah(Param2, Param3, Param4, Param5)
 				$kawal->{$url[1]}($url[2], $url[3], $url[4], $url[5], $url[6]);
 				break;
 
-				case 6:
-				//Kawal->Kaedah(Param2, Param3, Param4, Param5)
+				case 6: # Kawal->Kaedah(Param2, Param3, Param4, Param5)
 				$kawal->{$url[1]}($url[2], $url[3], $url[4], $url[5]);
 				break;
 	 
-				case 5:
-				//Kawal->Kaedah(Param2, Param3, Param4)
+				case 5: # Kawal->Kaedah(Param2, Param3, Param4)
 				$kawal->{$url[1]}($url[2], $url[3], $url[4]);
 				break;
 	 
-				case 4:
-				//Kawal->Kaedah(Param2, Param3)
+				case 4: # Kawal->Kaedah(Param2, Param3)
 				$kawal->{$url[1]}($url[2], $url[3]);
 				break;
 	 
-				case 3:
-				//Kawal->Kaedah(Param2)
+				case 3: # Kawal->Kaedah(Param2)
 				$kawal->{$url[1]}($url[2]);
 				break;
 	 
-				case 2:
-				//Kawal->Kaedah()
+				case 2: # Kawal->Kaedah()
 				$kawal->{$url[1]}();
 				break;
 	 
