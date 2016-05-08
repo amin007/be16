5cf7c63ef1ea59f7aca42e38c6b5311523065778
Amin PO Muar <helmi007@yahoo.com>
2016-05-08 11:04:19 +0800

    tambah Tanya/rangkabaru_tanya.php

---
+++ Aplikasi/Tanya/rangkabaru_tanya.php
@@ -0,0 +1,142 @@
+<?php
+namespace Aplikasi\Tanya; //echo __NAMESPACE__; 
+class Rangkabaru_Tanya extends \Aplikasi\Kitab\Tanya
+{
+#==========================================================================================
+	public function __construct() 
+	{
+		parent::__construct();
+	}
+	
+	public function medanUbah2($cariID)
+	{
+		$senaraiMedan = 'no,Nama_Penuh nama,email,nohp';
+		
+		# pulangkan pemboleubah
+		return $senaraiMedan;
+	}
+	
+	public function tatasusunanUbah2($jadual, $medan, $cari, $susun) 
+	{
+		# ada nilai
+		$hasil = array ( '0' => array (
+				        'no' => '2',
+				      'nama' => 'Fulan Bin Fulan',
+				     'email' => 'fulan@mail.com',
+				      'nohp' => '0123456789',
+				'keterangan' => '',					  
+				   'jantina' => 'lelaki',
+				  'password' => '123abd456',
+				    'terima' => '2016-03-30',
+				     'hasil' => '123654',
+				   'belanja' => '223654',
+				      'gaji' => '323654',
+				      'aset' => '423654',
+				      'staf' => '523654',
+				      'stok' => '623654',
+				));
+		
+		# tiada nilai
+		$hasil2 = array();
+		
+		# pulangkan pemboleubah
+		return $hasil;
+	}
+
+	public function tatasusunanUbah2A($jadual, $medan, $cari, $susun) 
+	{
+		# ada nilai - cantum semua tatasusunan dalam satu
+		$hasil = array (
+			'msic2008' => array (
+				0 => array (
+						'S' => 'S',
+						'msic2000' => '93099p',
+						'msic' => '96094',
+						'keterangan' => 'Perkhidmatan jagaan haiwan(2)',
+						'notakaki' => '(2) Termasuk: penumpangan, perapian, mendudukkan dan melatih binatang peliharaan',
+					),
+				),
+			'msic_v1' => array (
+				0 => array (
+						'msic' => '96094',
+						'kp' => '85',
+						'staf' => NULL,
+						'keterangan' => 'Perkhidmatan jagaan haiwan',
+						'notakaki' => 'Pet care services INCLUDE boarding, grooming, sitting and training pets '
+								. 'NOT INCLUDE veterinary activities, see 7500 activities of fitness centres, see 93118',
+					),
+			),
+			'msic_bandingan' => array (
+				0 => array (
+						'sv_newss' => '332',
+						'sv_sidap' => '85',
+						'msic2000p' => '93099p',
+						'msic2000' => '93099',
+						'msic' => '96094',
+						'keterangan' => 'Aktiviti Perkhidmatan Persendirian',
+						'Sektor' => 'Perkhidmatan (Lain-lain)',
+					),
+			),
+			'msic2000' => array (),
+			'msic2000_notakaki' => array (),
+		);
+		
+		# ada nilai - pecah tatasusunan kepada beberapa bahagian
+		$hasil1['satu'] = array ( 
+			'0' => array ('kira' => '1', 'A' => 'A1', 'B' => 'B1'),
+			'1' => array ('kira' => '1', 'A' => 'A1', 'B' => 'B1'),
+			'2' => array ('kira' => '1', 'A' => 'A1', 'B' => 'B1')
+			);
+		$hasil1['dua'] = array ( 
+			'0' => array ('kira' => '1', 'A' => 'A1', 'B' => 'B1'),
+			'1' => array ('kira' => '1', 'A' => 'A1', 'B' => 'B1'),
+			'2' => array ('kira' => '1', 'A' => 'A1', 'B' => 'B1')
+			);
+		$hasil1['tiga'] = array ( 
+			'0' => array ('kira' => '1', 'A' => 'A1', 'B' => 'B1'),
+			'1' => array ('kira' => '1', 'A' => 'A1', 'B' => 'B1'),
+			'2' => array ('kira' => '1', 'A' => 'A1', 'B' => 'B1')
+			);
+		
+		# tiada nilai
+		$hasil2 = array();
+		
+		# pulangkan pemboleubah
+		return $hasil;
+	}
+#==========================================================================================
+	public function binaPOST() 
+	{ 
+		# Set pemboleubah tatasusunan
+		$medan[] = array('newss'=>'000123456',
+			'nossm'=>null,
+			'CHECK_DIGIT'=>null,
+			'nama'=>'Biar Rahsia',
+			'operator'=>'',
+			'alamat1'=>'',
+			'alamat2'=>'',
+			'bandar'=>'',
+			'poskod'=>'',
+			'kp'=>'328',
+			'msic2008'=>'56107',
+			'fe'=>'x');
+		
+		# pulangkan pemboleubah
+		return $medan;
+	}
+	
+	public function binasql2() 
+	{ 
+		# Set pemboleubah tatasusunan
+		//$medan = array('newss','nossm','nama','operator','alamat1','alamat2','bandar','poskod','kp','msic2008','fe');
+		$dataPOST = $this->binaPOST();
+		echo '<pre>'; print_r($dataPOST) . '</pre>';
+		//foreach ($medan as $key2 => $nilai2):	
+		//endforeach;
+		//$senaraiMedan[] = array('kod_medan'=>'F21','2010'=>'');
+		
+		# pulangkan pemboleubah
+		//return $senaraiMedan;
+	}
+#==========================================================================================
+}
