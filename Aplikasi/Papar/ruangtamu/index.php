b249618b1e7291396a25ff47b83f90a108aa945a
Amin PO Muar <helmi007@yahoo.com>
2016-05-11 16:34:12 +0800

    paparkan $namaPegawai dan $namaPenuh

--- Aplikasi/Papar/ruangtamu/index.php
+++ Aplikasi/Papar/ruangtamu/index.php
@@ -1,3 +1,16 @@
+<div class="container">
 <hr><h1>Ruangtamu - Kita Tanya Apa Khabar</h1><hr>
+<div class="hero-unit">
+<p><?php
+$Sesi = new \Aplikasi\Kitab\Sesi();
+$Sesi->init();
+//echo '<pre>'; print_r($_SESSION) . '</pre>';
+echo 'namaPegawai=' . $Sesi->get('namaPegawai') . '<br>';
+echo 'namaPenuh=' . $Sesi->get('namaPenuh') . '<br>';
+//echo 'levelPegawai=' . $Sesi->get('levelPegawai') . '|';
+?></p>
+
+</div><!-- / class="hero-unit" -->
+</div><!-- / class="container" -->
 
 <?php //echo semakDataSesi(); ?>
