f4d7e7cec5ff323445a3ed052b46a2c3cb6d0784
Amin PO Muar <helmi007@yahoo.com>
2016-06-21 16:09:23 +0800

    tambah fungsi cetakA1

--- Aplikasi/Kawal/laporan.php
+++ Aplikasi/Kawal/laporan.php
@@ -112,6 +112,38 @@ class Laporan extends \Aplikasi\Kitab\Kawal
 		//$this->papar->baca('laporan/f3all', 1);
 		$this->papar->baca('laporan/f3responden', 1);
 	}
-
+#===============================================================================================
+	public function cetakA1($namaPegawai, $cariBatch, $item = 30, $ms = 1, $baris = 30)
+	{
+		$medan = $this->tanya->kumpulA1($item, $ms);# kumpul respon jadi medan sql
+		# set pembolehubah utama untuk sql
+		$jadual = 'be16_kawal';
+		$carian[] = array('fix'=>'like','atau'=>'WHERE','medan'=>'pegawai','apa'=>$namaPegawai);
+		$carian[] = array('fix'=>'like','atau'=>'AND','medan'=>'borang','apa'=>$cariBatch);
+		$carian[] = array('fix'=>'like','atau'=>'AND','medan'=>'respon','apa'=>'A1');
+		# tentukan bilangan mukasurat & jumlah rekod
+			$bilSemua = $this->tanya->kiraBaris//tatasusunanCari//cariSql
+			($jadual, $medan2 = '*', $carian, NULL);
+			# semak bilangan mukasurat & jumlah rekod
+			//echo '$bilSemua:' . $bilSemua . ', $item:' . $item . ', $ms:' . $ms . '<br>';
+			$jum = pencamSqlLimit($bilSemua, $item, $ms);
+		$susun[] = array_merge($jum, array('kumpul'=>null,'susun'=>'respon,kp,nama ASC') );
+		# tanya dalam sql 	
+		$this->papar->hasil = $this->tanya->cariSemuaData//cariSql
+			($jadual, $medan, $carian, $susun);
+		//echo '<pre>$hasil:'; print_r($this->papar->hasil) . '</pre>'; # semak data
+		
+		# Set pemboleubah utama
+        $this->papar->pegawai = senarai_kakitangan();
+		$this->papar->kiraSemuaBaris = $bilSemua;
+		$this->papar->item = $item;;
+		$this->papar->baris = $baris;
+		$this->papar->fe = $namaPegawai . '-' . $cariBatch;
+		$this->papar->kp = 'BE';
+				
+		# pergi papar kandungan
+		//$this->papar->baca('laporan/f3all', 1);
+		$this->papar->baca('laporan/f10', 1);//*/
+	}
 #==========================================================================================
 }
