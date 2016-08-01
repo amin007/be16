77054ecc5e9c46a634e4380f95eb16c5ba13f7fc
Amin PO Muar <helmi007@yahoo.com>
2016-08-01 15:35:58 +0800

    ringkaskan koding

--- Aplikasi/Papar/laporan/f3all.php
+++ Aplikasi/Papar/laporan/f3all.php
@@ -6,24 +6,23 @@ include 'diatas.php';
 	$namaOrg['pegawai'] = $this->fe;
 	if (count($this->hasil)==0):
 		$fields = null; 
-		$rows = null; 
-		$hasil = null; 
+		  $rows = null; 
+		 $hasil = null; 
 	else:
 		$fields = count($this->hasil[0]); 
-		$rows = count($this->hasil); 
-		$hasil = $this->hasil;
+		  $rows = count($this->hasil); 
+		 $hasil = $this->hasil;
 	endif;
 	$item = isset($this->item) ? $this->item : 30;
 	$baris = isset($this->baris) ? $this->baris : 30;
 	$ms = isset($this->ms) ? $this->ms : 1;
 	$hasil = $this->hasil;
-	$tajukRespon=array();
+	$tajukRespon = array();
 	/*# semak data
 	echo '<pre>$hasilLaporan:'; print_r($this->hasil) . '</pre>';
 	echo '<br>$baris:' . $rows . '|' . count($this->hasil) . '<br>';
 	echo '<br>$lajur:' . $fields . '|' . count($this->hasil[0]) . '<br>';
-	echo '<br>$kp:' . $kp . '<br>';
-	//*/
+	echo '<br>$kp:' . $kp . '<br>'; //*/
 
 if (count($this->hasil) == 0):
 	echo 'Tiada data';
