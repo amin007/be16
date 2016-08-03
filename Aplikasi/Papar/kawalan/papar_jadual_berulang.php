90d397a8bf230f87f9e506a9fea65c44e65c71d3
Amin PO Muar <helmi007@yahoo.com>
2016-08-03 16:24:38 +0800

    susun balik koding

--- Aplikasi/Papar/kawalan/papar_jadual_berulang.php
+++ Aplikasi/Papar/kawalan/papar_jadual_berulang.php
@@ -1,17 +1,16 @@
 Data dari rangka: <pre><?php print_r($this->senarai['data'][0]); ?></pre>
 <?php 
-	foreach ($this->cariApa as $myTable => $row)
+foreach ($this->cariApa as $myTable => $row)
+{
+	if ( count($row)==0 )
+		echo '';
+	else
 	{
-		if ( count($row)==0 )
-			echo '';
-		else
-		{
-			
 ?>
 <!-- Jadual <?php echo $myTable ?> ########################################### -->	
 <?php include 'papar_jadual_tambah.php'; ?>
 <!-- Jadual <?php echo $myTable ?> ########################################### -->		
 <?php
-		} // if ( count($row)==0 )
-	}
+	} # if ( count($row)==0 )
+}
 ?>	
