91b679325be711b632504252c220c8a85f4613b2
Amin PO Muar <helmi007@yahoo.com>
2016-08-03 12:45:29 +0800

    papar data $row[0]

--- Aplikasi/Papar/kawalan/papar_jadual_berulang.php
+++ Aplikasi/Papar/kawalan/papar_jadual_berulang.php
@@ -4,7 +4,9 @@ foreach ($this->cariApa as $myTable => $row)
 	if ( count($row)==0 )
 		echo '';
 	else
-	{?>
+	{
+		?>Data dari rangka: <pre><?php print_r($row[0]); ?></pre><?php
+?>
 <!-- Jadual <?php echo $myTable ?> ########################################### -->	
 <?php include 'papar_jadual_tambah.php'; ?>
 <!-- Jadual <?php echo $myTable ?> ########################################### -->		
