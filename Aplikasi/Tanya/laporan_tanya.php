9e39a138cbc8374b8e859ddc6e98e62c42404fa0
Amin PO Muar <helmi007@yahoo.com>
2016-08-14 14:36:33 +0800

    1. buang `pegawai` 2. tukar - kepada <br>

--- Aplikasi/Tanya/laporan_tanya.php
+++ Aplikasi/Tanya/laporan_tanya.php
@@ -299,10 +299,10 @@ class Laporan_Tanya extends \Aplikasi\Kitab\Tanya
 		$senaraiMedan = array(
 			0 => 'kod',
 			1 => 'f2',
-			2 => 'concat_ws(" | ",`respon`,`nota`,`pegawai`) as respon',
+			2 => 'concat_ws(" | ",`respon`,`nota`) as respon',
 			//2 => 'concat_ws(" | ",`posdaftar`,`posdaftar_terima`) as respon',
 			3 => 'concat_ws("-",`nama`) as nama,'
-			. 'concat_ws("-",`kp`,`msic2008`) as kp,'
+			. 'concat_ws("<br>",`kp`,`msic2008`) as kp,'
 			. 'concat_ws(" ","<input type=\"checkbox\">",alamat1,alamat2) as utama,'
 			. 'concat_ws("",`newss`) as newss,'
 			. 'concat_ws(" ","(",jalan,")<br>",orang_a,notel_a,nofax_a,esurat_a'
