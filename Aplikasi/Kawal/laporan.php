ea122c56619e3d58e507d4f742a13b2a59fe5a49
Amin PO Muar <helmi007@yahoo.com>
2016-08-23 08:17:05 +0800

    tambah fungsi cdaerah

--- Aplikasi/Kawal/laporan.php
+++ Aplikasi/Kawal/laporan.php
@@ -145,6 +145,39 @@ class Laporan extends \Aplikasi\Kitab\Kawal
 		$this->papar->baca('laporan/f3responden', null, 1);
 	}
 #===============================================================================================
+	public function cdaerah($namaPegawai, $cariBatch, $item = 30, $ms = 1, $baris = 30)
+	{
+		$medan = $this->tanya->kumpulAlamat($item, $ms);# kumpul respon jadi medan sql
+		# set pembolehubah utama untuk sql
+		$jadual = 'be16_kawal';
+		$cariBatch = 'segamat';
+		//$carian[] = array('fix'=>'like','atau'=>'WHERE','medan'=>'pegawai','apa'=>$namaPegawai);
+		$carian[] = array('fix'=>'%like%','atau'=>'WHERE','medan'=>'bandar','apa'=>$cariBatch);
+		# tentukan bilangan mukasurat & jumlah rekod
+			$bilSemua = $this->tanya->kiraBaris//tatasusunanCari//cariSql
+			($jadual, $medan2 = '*', $carian, NULL);
+			# semak bilangan mukasurat & jumlah rekod
+			//echo '$bilSemua:' . $bilSemua . ', $item:' . $item . ', $ms:' . $ms . '<br>';
+			$jum = pencamSqlLimit($bilSemua, $item, $ms);
+		$susun[] = array_merge($jum, array('kumpul'=>null,'susun'=>'jalan ASC, nama ASC') );
+		# tanya dalam sql 	
+		$this->papar->hasil = $this->tanya->cariSemuaData//cariSql
+			($jadual, $medan, $carian, $susun);
+		//echo '<pre>$hasil:'; print_r($this->papar->hasil) . '</pre>'; # semak data
+		
+		# Set pemboleubah utama
+        $this->papar->pegawai = senarai_kakitangan();
+		$this->papar->kiraSemuaBaris = $bilSemua;
+		$this->papar->item = $item;;
+		$this->papar->baris = $baris;
+		$this->papar->fe = $namaPegawai . '-' . $cariBatch;
+		$this->papar->kp = 'BE';
+				
+		# pergi papar kandungan
+		//$this->papar->baca('laporan/f3all', null, 1);
+		$this->papar->baca('laporan/f3responden', null, 1);
+	}
+#===============================================================================================
 	public function cetakA1($namaPegawai, $cariBatch, $item = 30, $ms = 1, $baris = 30)
 	{
 		$medan = $this->tanya->kumpulA1($item, $ms);# kumpul respon jadi medan sql
