8c7b7b81cd42f879e7541481d0a6774394aee4d8
Amin Ledang2 <al.amin007@yahoo.com>
2016-09-11 15:01:27 +0800

    tutup debug balik

--- Aplikasi/Kawal/operasi.php
+++ Aplikasi/Kawal/operasi.php
@@ -103,9 +103,9 @@ class Operasi extends \Aplikasi\Kitab\Kawal
 		endif;
 
 		# semak pembolehubah $this->papar->cariApa
-		echo '<pre>', print_r($this->papar->cariApa, 1) . '</pre><br>';
+		//echo '<pre>', print_r($this->papar->cariApa, 1) . '</pre><br>';
 
-		/*# pergi papar kandungan
+		# pergi papar kandungan
 		$jenis = $this->papar->pilihTemplate($template=0);
 		$this->papar->bacaTemplate(
 		//$this->papar->paparTemplate(
