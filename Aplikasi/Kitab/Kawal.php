3ab86071f63ca0b6bdb83f5871905a6779791fa8
Amin Ledang2 <al.amin007@yahoo.com>
2016-11-27 13:33:15 +0800

    buang space tab

--- Aplikasi/Kitab/Kawal.php
+++ Aplikasi/Kitab/Kawal.php
@@ -8,25 +8,25 @@ class Kawal
 		//echo '<br>class Kawal';
 		$this->papar = new \Aplikasi\Kitab\Papar();
 	}
-#==========================================================================================	
+#==========================================================================================
 	public function jemaahTaskil($nama) 
 	{
 		$failTanya = GetMatchingFiles(GetContents(TANYA),$nama . '_tanya.php');
 		$tanya = $failTanya[0];
 		/*echo '<br> class Kawal :: $nama : ' . $nama . '|';
 		echo 'TANYA->' . TANYA . '';
-		echo '<pre>$failTanya->'; print_r($failTanya) . '</pre>';
+		echo '<pre>$failTanya->'; print_r($failTanya); echo '</pre>';
 		echo '$tanya->' . $tanya . '<br>';
 		//*/
-		
-		if (file_exists($tanya)) 
+
+		if (file_exists($tanya))
 		{
 			$tanyaNama = '\\Aplikasi\Tanya\\' . huruf('Besar', $nama) . '_Tanya';
 			//echo '<br>$tanyaNama->' . $tanyaNama . '<br>';
-			
+
 			require_once $tanya;
 			$this->tanya = new $tanyaNama();
-			
+
 			//if (class_exists($tanyaNama)) echo '<br>class ' . $tanyaNama . ' wujud<br>';
 			//else echo '<br>class ' . $tanyaNama . ' tak wujud<br>';
 		}//*/
