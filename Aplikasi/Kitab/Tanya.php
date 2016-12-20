3aea427fa62789889fc56eacdc1037624d8aa7f1
Amin Ledang2 <al.amin007@yahoo.com>
2016-12-20 11:18:15 +0800

    tukar $cariApa kepada $cari

--- Aplikasi/Kitab/Tanya.php
+++ Aplikasi/Kitab/Tanya.php
@@ -78,13 +78,13 @@ class Tanya
 		else:
 			foreach ($carian as $key=>$value)
 			{
-				   $atau = isset($carian[$key]['atau'])  ? $carian[$key]['atau'] . ' ' : null;
-				  $medan = isset($carian[$key]['medan']) ? $carian[$key]['medan']      : null;
-				    $fix = isset($carian[$key]['fix'])   ? $carian[$key]['fix']        : null;
-				$cariApa = isset($carian[$key]['apa'])   ? $carian[$key]['apa']        : null;
-				  $akhir = isset($carian[$key]['akhir']) ? $carian[$key]['akhir']      : null;
-				//echo "\r$key => ($fix) $atau $medan -> '$cariApa' |";
-				$where .= $this->jika($fix,$atau,$medan,$cariApa,$akhir);
+				 $atau = isset($carian[$key]['atau'])  ? $carian[$key]['atau'] . ' ' : null;
+				$medan = isset($carian[$key]['medan']) ? $carian[$key]['medan']      : null;
+				  $fix = isset($carian[$key]['fix'])   ? $carian[$key]['fix']        : null;
+				 $cari = isset($carian[$key]['apa'])   ? $carian[$key]['apa']        : null;
+				$akhir = isset($carian[$key]['akhir']) ? $carian[$key]['akhir']      : null;
+				//echo "\r$key => ($fix) $atau $medan -> '$cari' |";
+				$where .= $this->jika($fix,$atau,$medan,$cari,$akhir);
 			}
 		endif;
 
