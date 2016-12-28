ce8b91e8d8dc46fccfb2d2d53f82b897c058c7b1
Amin Ledang2 <al.amin007@yahoo.com>
2016-12-28 12:24:53 +0800

    1. pecah fungsi pada() kepada beberapa fungsi lain

--- Aplikasi/Kawal/cari.php
+++ Aplikasi/Kawal/cari.php
@@ -24,7 +24,7 @@ class Cari extends \Aplikasi\Kitab\Kawal
 		//$this->papar->baca
 		$this->papar->bacaTemplate
 		//$this->papar->paparTemplate
-			($this->_folder . '/index', $jenis, 0); # $noInclude=0		
+			($this->_folder . '/index', $jenis, 0); # $noInclude=0
 	}
 #==========================================================================================
 	public function idnama()
@@ -80,7 +80,7 @@ class Cari extends \Aplikasi\Kitab\Kawal
     }
 
 	public function tentang($apa, $bil=1, $mesej=null) 
-	{	
+	{
 		/* fungsi ini memaparkan borang
 		 * echo 'mana ko pergi daa lokaliti($negeri)<br>';
 		 */
@@ -106,14 +106,11 @@ class Cari extends \Aplikasi\Kitab\Kawal
 			($this->_folder . '/index', $jenis, 0); # $noInclude=0
 	}
 
-	function pada($bil = 400, $muka = 1) 
+#==========================================================================================
+	function setPembolehUbah($bil, $posmen)
 	{
-		/* fungsi ini memaparkan hasil carian
-		 * untuk jadual msic2000 dan msic2008
-		 */
-
 		$had = '0, ' . $bil; # setkan $had untuk sql
-		$kira = pecah_post($_POST); # echo '<pre>$kira->'; print_r($kira); echo '</pre>';
+		$kira = $this->pecah_post($posmen); // echo '<pre>$kira->'; print_r($kira); echo '</pre>';
 		# setkan pembolehubah dulu
 		$namajadual = isset($_POST['namajadual']) ? $_POST['namajadual'] : null;
 		$susun = isset($_POST['susun']) ? $_POST['susun'] : 1;
@@ -123,163 +120,184 @@ class Cari extends \Aplikasi\Kitab\Kawal
 		$semak2 = isset($_POST['cari'][2]) ? $_POST['cari'][2] : null;
 		$atau = isset($_POST['atau']) ? $_POST['atau'] : null;
 		$this->papar->cariNama = null;
+		$this->papar->carian = $carian;
 		//echo '<pre>$_POST->', print_r($_POST, 1) . '</pre>';
 		//echo '$bil=' . $bil. '<br>$muka=' . $muka . '<br>';
 		//echo '$pilih=' . $pilih. '<br>$semak=' . $semak . '<br>';
+		//list($had,$kira,$namajadual,$susun,$carian,$pilih,$semak,$semak2,$atau) 
+		//list($had,$kira,$namajadual)
+
+		return array($had,$kira,$namajadual,$semak,$semak2);
+	}
+##============================================================================================
+	function pecah_post()
+	{
+		$papar['pilih'] = isset($_POST['pilih']) ? $_POST['pilih'] : null;
+		$papar['cari'] = isset($_POST['cari']) ? $_POST['cari'] : null;
+		$papar['fix'] = isset($_POST['fix']) ? $_POST['fix'] : null;
+		$papar['atau'] = isset($_POST['atau']) ? $_POST['atau'] : null;
+
+		$kira['pilih'] = count($papar['pilih']);
+		$kira['cari'] = count($papar['cari']);
+		$kira['fix'] = count($papar['fix']);
+		$kira['atau'] = count($papar['atau']);
+
+		return $kira;
+	}
+##============================================================================================
+	function pada($bil = 400, $muka = 1) 
+	{
+		# fungsi ini memaparkan hasil carian untuk jadual msic2000 dan msic2008
+		list($had,$kira,$namajadual,$semak,$semak2) = $this->setPembolehUbah($bil, $_POST);
 
 		if (!isset($_POST['atau']) && isset($_POST['pilih'][2]))
-		{
-			//echo '1)$namajadual=' . $namajadual . '<br>';
 			$mesej = 'tak isi atau-dan pada carian';
-			$lokasi = ($namajadual=='johor') ? 'lokaliti/' : 'semua/';
-		}
 		elseif ( (empty($semak) || ( empty($semak2) 
 			&& $namajadual=='johor') ) ) 
-		{
-			//echo '2)$namajadual=' . $namajadual . '<br>';
 			$mesej = 'tak isi pada carian';
-			$lokasi = ($namajadual=='johor') ? 'lokaliti/' : 'semua/';
-		}
-		elseif (!empty($namajadual) && $namajadual=='msic') 
-		{
-			//echo '3)$namajadual=' . $namajadual . '<br>';
-			$jadual = dpt_senarai('msicbaru');
-			# mula cari $cariID dalam $jadual
-			foreach ($jadual as $key => $namaPanjang)
-			{# mula ulang table
-				$myTable = substr($namaPanjang, 16);
-				# senarai nama medan
-				$medan = ($myTable=='msic2008') ? 
-					'seksyen S,bahagian B,kumpulan Kpl,kelas Kls,' .
-					'msic2000,msic,keterangan,notakaki' 
-					: '*'; 
-				$this->papar->cariNama[$myTable] = $this->tanya
-					->cariPOST($namaPanjang, $medan, $kira, $had);
+		elseif (!empty($namajadual)) 
+			$mesej = $this->paparJadual($namajadual,$kira,$had);//*/
 
-			}# tamat ulang table
-
-			$this->papar->carian=$carian;
-			$mesej = null; $lokasi = null;
-		}
-		elseif (!empty($namajadual) && $namajadual=='produk') 
-		{
-			$jadual = dpt_senarai('produk');
-			# mula cari $cariID dalam $jadual
-			foreach ($jadual as $key => $namaPanjang)
-			{# mula ulang table
-				$myTable = substr($namaPanjang, 16); //echo "<br>4) $myTable";
-				# senarai nama medan
-				$medan = ($myTable=='kodproduk_aup') ? 
-					'bil,substring(kod_produk_lama,1,5) as msic,kod_produk_lama,' .
-					'kod_produk,unit_kuantiti unit,keterangan,keterangan_bi,aup,min,max' 
-					: '*'; 
-				$this->papar->cariNama[$myTable] = $this->tanya
-				->cariPOST($namaPanjang, $medan, $kira, $had);
-			}# tamat ulang table
-
-			# papar jadual kod unit
-			$unitPanjang = 'pom_dataekonomi.kodproduk_unitkuantiti';
-			$unit = 'unitkuantiti';
-				$this->papar->cariNama[$unit] = $this->tanya->
-					cariSemuaData($unitPanjang, '*', null, null);
-
-			$this->papar->carian=$carian;
-			$mesej = null; $lokasi = null;
-		}
-		elseif (!empty($namajadual) && $namajadual=='syarikat') 
-		{
-			//echo '5)$namajadual=' . $namajadual . '<br>';
-			$jadual = dpt_senarai('syarikat');
-			# mula cari $cariID dalam $jadual
-			foreach ($jadual as $key => $myTable)
-			{# mula ulang table
-				$this->papar->cariNama[$myTable] = $this->tanya
-					->cariPOST($myTable, $medan = '*', $kira, $had);
-			}# tamat ulang table
-
-			$this->papar->carian=$carian;
-			$mesej = null; $lokasi = null;
-		}
-		elseif (!empty($namajadual) && $namajadual=='johor') 
-		{
-			//`KOD NEGERI`, `NEGERI`,
-			//echo '6)$namajadual=' . $namajadual . '<br>';
-				# senarai nama medan
-				$medanAsal = '`KOD NGDBBP 2010`,`PEJABAT OPERASI`,' .
-				"\r" . ' concat(`KOD DAERAH BANCI`,"-",`DAERAH BANCI`," | ",`NEGERI`) as DB,' .
-				"\r" . ' concat(`KOD STRATA`,"-",`STRATA`) as STRATA,' .
-				"\r" . ' concat(`KOD MUKIM`,"-",`MUKIM`) as MUKIM,' .
-				"\r" . ' concat(`KOD BP`,"-",`DAERAH PENTADBIRAN`) as DAERAH,' .
-				"\r" . ' concat(`KOD PBT`,"-",`PIHAK BERKUASA TEMPATAN`) as PBT,' .
-				"\r" . ' concat(`KOD BDR`,"-",`NAMA BANDAR`) as BANDAR,' .
-				"\r" . '`DESKRIPSI (LOKALITI STATISTIC KAWKECIL)`, `LOKALITI UNTUK INDEKS`'; 
-				# senarai nama medan
-				$medanBaru = '`KOD NGDBBP 2010`,' .
-				//"\r" . ' concat("01",`no_db`, `no_bp_baru`) as `KodNGDBBP`,' .
-				"\r" . ' `kod_strata` as STRATA, NEGERI,' .
-				"\r" . ' concat(`KodMukim`,"-",`Mukim`) as MUKIM,' .
-				"\r" . ' concat(`KodDP`,"-",`Daerah Pentadbiran`) as DAERAH,' .
-				"\r" . ' concat(`KodPBT`,"-",`PBT`) as PBT,' .
-				"\r" . ' `catatan`, `kawasan`,' .
-				"\r" . ' `LOKALITI UNTUK INDEKS`'; 
-
-			$jadual = dpt_senarai('johor');
-			# mula cari $cariID dalam $jadual
-			foreach ($jadual as $key => $myTable)
-			{# mula ulang table
-				
-				$medan = ($myTable=='pom_lokaliti.johor') ? 
-					$medanAsal : $medanBaru;
-				$myJadual = ($myTable=='pom_lokaliti.johor') ? 
-					'JOHOR':'LK-JOHOR';
-				$this->papar->cariNama[$myJadual] = $this->tanya
-					->cariPOST($myTable, $medan, $kira, $had);
-
-			}# tamat ulang table
-			
-			$this->papar->carian=$carian;
-			$mesej = null; $lokasi = null;
-		}
-		elseif (!empty($namajadual) && $namajadual=='data_mm_prosesan') 
-		{
-			//echo '7)$namajadual=' . $namajadual . '<br>';
-			$jadual = dpt_senarai('prosesan');
-			# mula cari $cariID dalam $jadual
-			foreach ($jadual as $key => $myTable)
-			{# mula ulang table
-				$this->papar->cariNama[$myTable] = $this->tanya
-					->cariPOST($myTable, $medan = '*', $kira, $had);
-			}# tamat ulang table
-			
-			$this->papar->carian = $carian;
-			$mesej = null; $lokasi = null;
-		}
-
-		/* echo '<pre>'; # semak output
+		//$this->semakOutput(); 
+		# pergi papar kandungan
+		$this->paparOutput($mesej, $namajadual);
+	}
+##============================================================================================
+	private function semakOutput()
+	{
+		echo '<pre>'; # semak output
 		//echo 'Patah balik ke ' . $lokasi . $mesej . $namajadual . '<hr>';
 		echo '$this->papar->cariNama:'; print_r($this->papar->cariNama);
 		//echo '$this->papar->carian : ' . $this->papar->carian . '<br>'
 		//	. '$this->papar->apa : ' . $this->papar->apa . '<br>';
-		echo '</pre>';
-		//*/
-
-		# paparkan ke fail cari/$namajadual.php
+		echo '</pre>';//*/
+	}
+##============================================================================================
+	private function paparOutput($mesej, $namajadual)
+	{
 		if ($mesej != null ) 
 		{
 			$_SESSION['mesej'] = $mesej;
+			$lokasi = ($namajadual=='johor') ? 'lokaliti/' : 'semua/';
 
 			//echo 'Patah balik ke ' . $lokasi . $mesej . '<hr>' . $data;
 			header('location:' . URL . 'cari/' . $lokasi . $namajadual . '/2');
 		}
 		else 
 		{
-			//echo 'Tak patah balik';
 			$this->papar->primaryKey = 'newss';
-			$this->papar->baca('cari/cari', 0);	
+			//$this->papar->baca('cari/cari', 0);
+			# pergi papar kandungan
+			$jenis = $this->papar->pilihTemplate($template=0);
+			//$this->papar->baca
+			$this->papar->bacaTemplate
+			//$this->papar->paparTemplate
+				($this->_folder . '/jumpaCari', $jenis, 0); # $noInclude=0
 		}
-		//*/
 	}
+##============================================================================================
+	private function pilihJadual($namajadual)
+	{
+		if ($namajadual=='msic')
+			$jadual = dpt_senarai('msicbaru');
+		elseif($namajadual=='produk')
+			$jadual = dpt_senarai('produk');
+		elseif($namajadual=='johor')
+			$jadual = dpt_senarai('johor');
+		elseif($namajadual=='syarikat')
+			$jadual = dpt_senarai('syarikat');
+		elseif($namajadual=='data_mm_prosesan')
+			$jadual = dpt_senarai('prosesan');
 
+		//echo "\$namajadual = '$namajadual' <hr>";
+		//echo '<pre>$jadual::'; print_r($jadual); echo '<pre>';
+
+		return $jadual;
+	}
+##============================================================================================
+	private function paparJadual($namajadual,$kira,$had)
+	{
+		$jadual = $this->pilihJadual($namajadual);
+
+		# mula cari $cariID dalam $jadual
+		foreach ($jadual as $key => $namaPanjang)
+		{# mula ulang table
+			# jika untuk msic dan produk sahaja
+			$myTable = $this->keratNamaPanjang($namajadual,$namaPanjang);
+			//echo "\$namaPanjang = $namaPanjang <hr>";
+			//echo "\$myTable = $myTable <hr>";
+			# pilih nama medan berasaskan $myTable 
+			$medan = $this->pilihNamaMedan($myTable);
+			$this->papar->cariNama[$myTable] = $this->tanya
+				->cariPOST($namaPanjang, $medan, $kira, $had);
+		}# tamat ulang table
+
+		if($namajadual=='produk')
+			$this->tambahJadualLagi();
+
+		return $mesej = null; # pulangkan nilai
+	}
+##============================================================================================
+	private function tambahJadualLagi()
+	{# papar jadual kod unit
+		$namaPanjang = 'pom_dataekonomi.kodproduk_unitkuantiti';
+		$myTable = 'unitkuantiti';
+		$this->papar->cariNama[$myTable] = $this->tanya->
+			cariSemuaData($namaPanjang, '*', null, null);
+	}
+##============================================================================================
+	private function keratNamaPanjang($namajadual,$namaPanjang)
+	{
+		if( in_array($namajadual,array('msic','produk') ) )
+				$myTable = substr($namaPanjang, 16);
+		else	$myTable = $namaPanjang;
+		
+		return $myTable;
+	}
+##============================================================================================
+	private function pilihNamaMedan($myTable)
+	{
+		if ($myTable=='msic2008') 
+		{
+			$medan = 'seksyen S,bahagian B,kumpulan Kpl,kelas Kls,'
+			. 'msic2000,msic,keterangan,notakaki'; 
+		}
+		elseif ($myTable=='kodproduk_aup') 
+		{
+			$medan = 'bil,substring(kod_produk_lama,1,5) as msic,kod_produk_lama,'
+			. 'kod_produk,unit_kuantiti unit,keterangan,keterangan_bi,aup,min,max' 
+			. ''; 
+		}
+		elseif ($myTable=='pom_lokaliti.johor') 
+		{
+			$medan = '`KOD NGDBBP 2010`,`PEJABAT OPERASI`,'
+			. "\r" . ' concat(`KOD DAERAH BANCI`,"-",`DAERAH BANCI`," | ",`NEGERI`) as DB,'
+			. "\r" . ' concat(`KOD STRATA`,"-",`STRATA`) as STRATA,'
+			. "\r" . ' concat(`KOD MUKIM`,"-",`MUKIM`) as MUKIM,'
+			. "\r" . ' concat(`KOD BP`,"-",`DAERAH PENTADBIRAN`) as DAERAH,'
+			. "\r" . ' concat(`KOD PBT`,"-",`PIHAK BERKUASA TEMPATAN`) as PBT,'
+			. "\r" . ' concat(`KOD BDR`,"-",`NAMA BANDAR`) as BANDAR,'
+			. "\r" . '`DESKRIPSI (LOKALITI STATISTIC KAWKECIL)`, `LOKALITI UNTUK INDEKS`'; 
+		}
+		elseif ($myTable=='pom_lokaliti.lk-johor') 
+		{
+			$medan = '`KOD NGDBBP 2010`,'
+			//. "\r" . ' concat("01",`no_db`, `no_bp_baru`) as `KodNGDBBP`,'
+			. "\r" . ' `kod_strata` as STRATA, NEGERI,'
+			. "\r" . ' concat(`KodMukim`,"-",`Mukim`) as MUKIM,'
+			. "\r" . ' concat(`KodDP`,"-",`Daerah Pentadbiran`) as DAERAH,'
+			. "\r" . ' concat(`KodPBT`,"-",`PBT`) as PBT,'
+			. "\r" . ' `catatan`, `kawasan`,'
+			. "\r" . ' `LOKALITI UNTUK INDEKS`'; 
+		}
+		else 
+		{
+			$medan = '*';
+		}
+
+		return $medan; # pulangkan nilai
+	}
+##============================================================================================
 	public function syarikat($carilah = null)
 	{
 		$cari = bersih($_GET['cari']); //echo "URL \$cari = $cari <br> GET \$cari = $carilah";
