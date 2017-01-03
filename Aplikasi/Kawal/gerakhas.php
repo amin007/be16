d83cb1724872f3165bc41808a55fc3d189911e54
Amin Ledang2 <al.amin007@yahoo.com>
2017-01-03 17:02:37 +0800

    buat fail Aplikasi/Kawal/gerakhas.php

---
+++ Aplikasi/Kawal/gerakhas.php
@@ -0,0 +1,213 @@
+<?php
+namespace Aplikasi\Kawal; //echo __NAMESPACE__; 
+class Gerakhas extends \Aplikasi\Kitab\Kawal
+{
+#==========================================================================================
+	function __construct() 
+	{
+		//echo '<br>class Crud extends Kawal';
+		parent::__construct();
+		//\Aplikasi\Kitab\Kebenaran::kawalMasuk();
+		//\Aplikasi\Kitab\Kebenaran::kawalKeluar();
+		$this->_folder = 'gerakhas';
+		$this->medanData = '*';
+	}
+
+	function index() 
+	{
+		# Set pemboleubah utama
+		$this->papar->Tajuk_Muka_Surat='Enjin CRUD';
+		# pergi papar kandungan
+		$jenis = $this->papar->pilihTemplate($template=0);
+		//$this->papar->baca(
+		$this->papar->bacaTemplate(
+		//$this->papar->paparTemplate(
+			$this->_folder . '/index',$jenis,0); # $noInclude=0
+		
+	}
+#==========================================================================================	
+	public function paparlimit($cariID = null, $cariApa = null) 
+	{
+		# Set pemboleubah utama
+		$this->papar->Tajuk_Muka_Surat='Enjin CRUD';
+		$item = 1000; $ms = 1;
+		# kod asas panggil sql
+			$medan = '*'; # papar semua medan
+			$jadual = '{jadual}';
+			$cari[] = array('fix'=>'x=','atau'=>'WHERE','medan'=>$cariID,'apa'=>$cariApa);
+			$jum2 = pencamSqlLimit(300, $item, $ms); #
+			$susun[] = array_merge($jum2, array('kumpul'=>null,'susun'=>null) );
+			# tanya Sql
+			$this->papar->senaraiApa = $this->tanya->tatasusunanUbah2A
+				//cariSemuaData //cariSql
+				($jadual, $medan, $cari, $susun = null);
+
+		/*# semak data
+		echo '<pre>';
+		//echo '<br>$this->papar->cariID:'; print_r($this->papar->cariID); 
+		//echo '<br>$this->papar->cariApa:'; print_r($this->papar->cariApa); 
+		echo '$this->papar->senaraiApa:<br>'; print_r($this->papar->senaraiApa);
+ 		echo '</pre>'; //*/
+		
+		# pergi papar kandungan
+		$this->papar->baca($this->_folder . '/papar');
+	}
+	   
+    public function ubah($cariID = null, $medanID = null, $jadualUbah = null) 
+    {//echo '<br>Anda berada di class Crud:ubah($cariID) extends \Aplikasi\Kitab\Kawal<br>';
+                
+        # senaraikan tatasusunan jadual dan setkan pembolehubah
+		$this->papar->lokasi = 'Enjin - Ubah';
+		$this->papar->_jadual = $jadualUbah;
+		$medanUbah = $this->tanya->medanUbah($cariID);
+		//$medanID = ''; $jadualUbah = ''; # 
+	
+		if (!empty($cariID)) 
+		{
+			$cari[] = array('fix'=>'like','atau'=>'WHERE','medan'=>$medanID,'apa'=>$cariID);
+			# 1. mula semak dalam jadual
+			$this->papar->senarai['data'] = $this->tanya->
+				//tatasusunanUbah2($jadualUbah, $medanUbah, $cari, $susun = null);
+				cariSemuaData($jadualUbah, $medanUbah, $cari, $susun = null);
+				//cariSql($jadualUbah, $medanUbah, $cari, $susun = null);
+
+			if(isset($this->papar->senarai['data'][0][$medanID])):
+				$this->papar->jumpa = $this->papar->senarai['data'][0][$medanID];
+				# cari data lain jika jumpa
+				$this->papar->_paparSahaja = $this->tanya->
+					//tatasusunanUbah2A($jadualUbah, $medanUbah, $cari, $susun = null);
+					cariSemuaData($jadualUbah, $medanUbah, $cari, $susun = null);
+					//cariSql($jadualUbah, $medanUbah, $cari, $susun = null);
+			else:
+				$this->papar->jumpa = '[tiada jumpa apa2]';
+			endif;
+			
+			$this->papar->cariID  = $medanID;
+			$this->papar->cariApa = $cariID;
+		}
+		else
+		{
+			$this->papar->senarai['data'] = array();
+			$this->papar->cariID  = '[mahu cari apa]';
+			$this->papar->cariApa = '[tiada id diisi]';
+			$this->papar->jumpa   = '[hendak cari apa kalau id tiada]';
+		}
+
+		/*echo '<pre>'; # semak data
+		echo '$this->papar->senarai:<br>'; print_r($this->papar->senarai); 
+		echo '<br>$this->papar->cariID:'; print_r($this->papar->cariID); 
+		echo '<br>$this->papar->cariApa:'; print_r($this->papar->cariApa); 
+		echo '<br>$this->papar->jumpa:'; print_r($this->papar->jumpa); 
+		echo '<br>$this->papar->_jadual:'; print_r($this->papar->_jadual); 
+		echo '</pre>'; //*/
+
+        # pergi papar kandungan
+        $this->papar->baca($this->_folder . '/ubah', 0);
+    }
+
+	public static function ubahCari()
+	{
+		//echo '<pre>$_GET->', print_r($_GET, 1) . '</pre>';
+		# bersihkan data $_POST
+		$input = bersih($_GET['cari']);
+		$dataID = str_pad($input, 12, "0", STR_PAD_LEFT);
+
+		# Set pemboleubah utama
+        $this->papar->lokasi = 'Enjin - Ubah';
+
+		# pergi papar kandungan
+		//echo '<br>location: ' . URL . $this->_folder . '/ubah/' . $dataID . '';
+		//header('location: ' . URL . $this->_folder . '/ubah/' . $dataID);
+	}
+#==========================================================================================
+	public function ubahSimpan($dataID)
+	{
+		$posmen = array();
+		$medanID = 'newss';
+		$senaraiJadual = array('be16_kawal');
+
+		foreach ($_POST as $myTable => $value)
+			if ( in_array($myTable,$senaraiJadual) )
+				foreach ($value as $kekunci => $papar)
+				{
+					$posmen[$myTable][$kekunci] = bersih($papar);
+					$posmen[$myTable][$medanID] = $dataID;
+				}
+
+		# ubahsuai $posmen
+		$posmen = $this->pecah5P($senaraiJadual[0], $posmen);
+		$posmen = $this->tanya->semakPosmen($senaraiJadual[0], $posmen);
+		//echo '<br>$dataID=' . $dataID . '<br>';
+		//echo '<pre>$_POST='; print_r($_POST) . '</pre>';
+		//echo '<pre>$posmen='; print_r($posmen) . '</pre>';
+
+		# mula ulang $senaraiJadual
+		foreach ($senaraiJadual as $kunci => $jadual)
+		{# mula ulang table
+			$this->tanya->ubahPDOSqlSimpan
+			//ubahPDOSimpan
+			($posmen[$jadual], $jadual, $medanID);
+		}# tamat ulang table
+
+		# pergi papar kandungan
+		//echo 'location: ' . URL . 'kawalan/ubah/' . $dataID;
+		header('location: ' . URL . 'kawalan/ubah/' . $dataID); //*/
+	}
+
+	function pecah5P($myTable, $posmen) 
+	{
+		$pecah5P = $posmen[$myTable]['pecah5P']; 
+
+		if (!empty($pecah5P))
+		{
+			$pos = explode(" ", $pecah5P);
+			  $posmen[$myTable]['hasil'] = str_replace( ',', '', bersih($pos[0]) );
+			$posmen[$myTable]['belanja'] = str_replace( ',', '', bersih($pos[1]) );
+			   $posmen[$myTable]['gaji'] = str_replace( ',', '', bersih($pos[2]) );
+			   $posmen[$myTable]['aset'] = str_replace( ',', '', bersih($pos[3]) );
+			   $posmen[$myTable]['staf'] = str_replace( ',', '', bersih($pos[4]) );
+			   $posmen[$myTable]['stok'] = str_replace( ',', '', bersih($pos[5]) );
+		}
+		else
+		{
+			foreach ($posmen as $jadual => $value)
+			foreach ($value as $kekunci => $papar)
+				$posmen[$myTable][$kekunci]= 
+					( in_array($kekunci,array('hasil','belanja','gaji','aset','staf','stok')) ) ?
+					str_replace( ',', '', bersih($papar) )# buang koma
+					: bersih($papar);
+		}
+
+		unset($posmen[$myTable]['pecah5P']);
+
+		/*# debug
+		echo '<pre>$hasil='; print_r($hasil); echo '</pre>';
+		echo '<pre>$pos='; print_r($pos); echo '</pre>';
+		echo '<pre>$posmen2='; print_r($posmen); echo '</pre>';//*/
+
+		return $posmen; # pulangkan nilai
+	}
+#==========================================================================================
+	public static function semakfail()
+	{
+		echo 'class gerakhas::semakfail()<br>';
+		# Set pemboleubah utama
+		$url = URL . 'sumber/fail/csv/';
+		$fail = 'odebe2016.csv'; //echo $url . $fail . '<hr>';
+		if( isset ($this->tanya) )
+			echo '$this->tanya wujud<br>';
+		else
+			echo '$this->tanya tidak wujud<br>';
+
+		//list($medan,$senaraiData) = $this->tanya->panggilFail($url, $fail);
+		# semak $bacaData
+		echo '$medan = ' . $medan . '<hr>';
+		echo '<pre>$senaraiData = ' . $senaraiData . '</pre><hr>';
+
+		# sql insert
+		//$this->tanya->tambahSqlBanyakNilai($myTable = 'nama_pegawai', $medan, $senaraiData); 
+		//echo '<pre>'; print_r($bacaData); echo '</pre>';
+		//*/
+	}
+#==========================================================================================
+}
