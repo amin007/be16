f0aca682efe63e846a48bcae183f353524d00797
Amin Ledang2 <al.amin007@yahoo.com>
2017-03-09 09:49:10 +0800

    1. ringkaskan tajuk 2. betulkan koding di if ($key=='newss')

--- Aplikasi/Papar/cari/jumpaCari.php
+++ Aplikasi/Papar/cari/jumpaCari.php
@@ -1,12 +1,11 @@
-<pre>
-<h1>...</h1>Anda mencari<?php 
+<h1>Senarai Syarikat | Anda mencari<?php 
 //echo '$this->carian:'; print_r($this->carian);
 $cari = ' | ';
 foreach ($this->carian as $kunci => $nilai)
 {
 	$cari .= ( count($nilai)==0 ) ? $nilai : $nilai . ' | ';
 }
-echo "$cari\rJadual\r";
+echo "$cari\r</h1>Jadual : ";
 //echo "\r" . '$this->cariNama:'; print_r($this->cariNama);
 foreach ($this->cariNama as $key => $value)
 {
@@ -14,11 +13,10 @@ foreach ($this->cariNama as $key => $value)
 	$key . ' Kosong | ' : $key . ' Ada | ';
 }
 ?>
-</pre>
 
 <?php
-//$papar = 'bawah';
-$papar = 'lintang';
+$papar = 'bawah';
+//$papar = 'lintang';
 
 if ($papar=='bawah')
 {// if ($papar=='bawah') 
@@ -39,7 +37,7 @@ for ($kira=0; $kira < count($row); $kira++)
 	if ( !$printed_headers ) # print the headers once: 
 	{##=============================================================
 		?><thead><tr>
-<th>#</th>
+<th>Tindakan</th><th>#</th>
 <?php
 		foreach ( array_keys($row[$kira]) as $tajuk ) 
 		{	# anda mempunyai kunci integer serta kunci rentetan
@@ -51,7 +49,7 @@ for ($kira=0; $kira < count($row); $kira++)
 		$printed_headers = true; 
 	} 
 #----//print the data row----------------------------------------------
-	?><tbody><tr><td><?php echo $kira+1 ?></td>
+	?><tbody><tr>
 <?php foreach ( $row[$kira] as $key=>$data ) :
 		pautan($kira, $key, $data, $myTable);
 	 endforeach; ?>
@@ -106,7 +104,7 @@ for ($kira=0; $kira < count($row); $kira++)
 {
 	if ( !$printed_headers ) # print the headers once: 
 	{
-		?><thead><tr><th><?php echo $myTable ?></th><?php
+		?><thead><tr><th>#</th><?php
 		foreach ( array_keys($row[$kira]) as $tajuk ) 
 		{ 
 			# anda mempunyai kunci integer serta kunci rentetan
@@ -116,11 +114,11 @@ for ($kira=0; $kira < count($row); $kira++)
 				?><th><?php echo $tajuk ?></th><?php
 			} 
 		}
-		?></tr></thead>
+		?><th><?php echo $myTable ?></th></tr></thead>
 <?php	$printed_headers = true; 
 	} 
-#---//print the data row-------------------------------------------
-	?><tbody><tr><td><?php echo $kira+1 ?></td><?php
+#---//print the data row------------------------------------------- 
+	?><tbody><tr><?php
 	foreach ( $row[$kira] as $key=>$data ) 
 	{
 		pautan($kira, $key, $data, $myTable);
@@ -146,6 +144,11 @@ for ($kira=0; $kira < count($row); $kira++)
 ?><?php
 function pautan($kira, $key, $data, $myTable)
 {
+	# set pembolehubah Sesi
+	$pengguna = \Aplikasi\Kitab\Sesi::get('namaPegawai');
+	$level = \Aplikasi\Kitab\Sesi::get('levelPegawai');
+	//echo "<br> \$pengguna : $pengguna | \$level = $level";
+
 	# butang 
 	$birutua = 'btn btn-primary btn-mini';
 	$birumuda = 'btn btn-info btn-mini';
@@ -153,13 +156,50 @@ function pautan($kira, $key, $data, $myTable)
 
 	if ($key=='newss')
 	{
+		//$b1 = URL . 'borang/tambah/' . $data;
+		if ($level=='feprosesan'):
+			$k1 = URL . 'prosesan/ubah/' . $data;
+			$btn =  $birumuda;
+			$a = '<i class="fa fa-pencil-square-o" aria-hidden="true"></i>Ubah2';
+		elseif ($myTable=='be16_rangkabaru'):
+			$k1 = URL . 'rangkabaru/masukdatadarirangka/1/' . $key . '/' . $data;
+			$btn =  $merah;
+			$a = '<i class="fa fa-pencil-square-o" aria-hidden="true"></i>Tambah2';
+		else:
+			$k1 = URL . 'kawalan/ubah/' . $data;
+			$btn = $birutua;
+			$a = '<i class="fa fa-pencil" aria-hidden="true"></i>Ubah1';
+		endif
+
 		?><td><?php 
 		/*?><a target="_blank" href="#" class="<?=$merah?>"><?=$level?></a><?php*/
 		?><a target="_blank" href="<?php echo $k1 ?>" class="<?=$btn?>"><?=$a?></a><?php
 		?></td><?php
+		?><td><?php echo $kira+1 ?></td><?php
+		?><td><?php echo $data ?></td><?php
 	}
 	else
 	{
 		?><td><?php echo $data ?></td><?php
 	}
 }
+
+	/*
+			if ($key=='newss')
+				$id = $data; 
+			?><td><?php echo $data ?></td><?php
+		 
+
+		$k1 = URL . 'kawalan/ubah/' . $id;
+		$p1 = URL . 'prosesan/ubah/' . $id;
+		//$p2 = URL . 'pendaftaran/buang/' . $id;
+		$k3 = URL . 'kawalan/cetak/' . $id;
+		//<a href="$p2" class="btn btn-danger btn-mini">
+		//<i class="icon-trash icon-white"></i> Buang</a>
+		?><td><?php
+		?><a href="<?php echo $k1 ?>" class="btn btn-primary btn-mini"><i class="icon-pencil icon-white"></i>Ubah</a><?php
+		?><a href="<?php echo $p1 ?>" class="btn btn-primary btn-mini"><i class="icon-pencil icon-white"></i>Proses</a><?php
+		?><a href="<?php echo $k3 ?>" class="btn btn-success btn-mini"><i class="icon-print icon-white"></i>Cetak</a><?php
+		?></td></tr></tbody>
+
+	*/
